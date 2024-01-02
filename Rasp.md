1a152c3dc30dadc00fe40ea46af87cf027e886eb
Daniele Carrozzino <carrozzino@feedbackitalia.it>
2024-01-02 23:59:12 +0100

    ..

---
+++ Rasp.md
@@ -0,0 +1,3 @@
+# Raspberry pi 2 W
+## Structure
+Left Part 3.3V - right part 5V
--- models/plants_analyzer.py
+++ models/plants_analyzer.py
@@ -4,7 +4,7 @@ class PlantAnalyzer():
 
     # Init yolov8 model
     def __init__(self) -> None:
-        self.model = YOLO('yolov8n.pt')
+        self.model = YOLO('best.pt')
 
     def getWebcamResult(self):
         result = self.model(source = 0, show = True, conf = 0.4, save = True)
