93f9305f6f04812e47360f0db71ea617ba05cce5
Daniele Carrozzino <carrozzino@feedbackitalia.it>
2024-01-03 11:27:55 +0100

    ..

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from firebase import storage, firestore, messaging
 from configuration import reader_conf as reader
 #from models import plants_analyzer as analyzer
-from raspberry import DHT11
+import DHT11
 import time
 
 def update_gardens(gardens, data):
