1a50ed5b9686fcadd5fcc43223896f2a644355da
Daniele Carrozzino <carrozzino@feedbackitalia.it>
2024-01-05 17:23:18 +0100

    test

--- test.py
+++ test.py
@@ -7,14 +7,4 @@ from adafruit_ads1x15.analog_in import AnalogIn
 # Create I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# Create ADC object
-ads = ADS.ADS1115(i2c)
 
-# Create analog input channels
-chan = AnalogIn(ads, ADS.P0)  # Change ADS.P0 to ADS.P1 for AIN1, ADS.P2 for AIN2, etc.
-
-# Read analog input continuously
-while True:
-    print("Voltage: {:.2f}V".format(chan.voltage))
-    print("ADC Value: {}".format(chan.value))
-    time.sleep(1)
