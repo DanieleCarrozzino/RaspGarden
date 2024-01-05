0801bfa0134c66deb2f207309fd6d7f171820e5c
Daniele Carrozzino <carrozzino@feedbackitalia.it>
2024-01-05 17:27:44 +0100

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-adc = ADS.ADS1115()
+adc = ADS.ADS1015(address=0x48, bus=1)
 
 # Create analog input channels
 chan = AnalogIn(adc, ADS.P0)  # Change ADS.P0 to ADS.P1 for AIN1, ADS.P2 for AIN2, etc.
