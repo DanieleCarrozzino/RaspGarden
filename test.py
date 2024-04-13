3fc8930df2124f441cec3205caf09d3bcb0f4331
Daniele Carrozzino <carrozzino@feedbackitalia.it>
2024-04-13 16:08:21 +0200

    .

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from raspberry.sensors import DHT11
 from raspberry.sensors import ADS1115
+import time
 
 sensor_moisture     = ADS1115.ADS1115Class()
 
 while(True):
     print(sensor_moisture.read())
+    time.sleep(1)
     pass
